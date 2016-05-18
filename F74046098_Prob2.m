71a59c91457e0f0f79a7dbe680c692d245b31237
cccccroge <pooopooo543@gmail.com>
2016-05-18 20:32:25 +0800

    Add files via upload

--- F74046098_Prob2.m
+++ F74046098_Prob2.m
@@ -1,9 +1,9 @@
 for i=-7:7
-   for j=-7:7 
-    PlotE(i,j);
+   for j=-7:7
+    PlotE(i,j)
    end
 end
 
-plot(0,0,'o');
+plot(0,0,'o')
 
 hold off;
--- PlotArrow.m
+++ PlotArrow.m
@@ -3,7 +3,7 @@ function[] = PlotArrow(a,b)
     x = [a(1),b(1)];
     y = [a(2),b(2)];
     line(x,y)
-    hold on;
+    hold on
     
     %put origin/vertical vectors together and get the one side of arrow
     O = [(b(1)-a(1)) , (b(2)-a(2))];
@@ -14,6 +14,7 @@ function[] = PlotArrow(a,b)
     x_arrow = [c(1),b(1)];
     y_arrow = [c(2),b(2)];
     line(x_arrow,y_arrow)
+    hold on
     
     %now use another vertical vectors to plot another side of arrow
     V = [O(2).*(-1),O(1)];      %another vertical vectors
@@ -23,7 +24,6 @@ function[] = PlotArrow(a,b)
     x_arrow = [c(1),b(1)];
     y_arrow = [c(2),b(2)];
     line(x_arrow,y_arrow)
-    
-    hold off;
+    hold on
 
 end
