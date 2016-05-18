b2f687fdcc829800ef855c3eeac9d0863596b95c
cccccroge <pooopooo543@gmail.com>
2016-05-18 19:45:21 +0800

    Add files via upload

---
+++ F74046098_Prob2.m
@@ -0,0 +1,9 @@
+for i=-7:7
+   for j=-7:7 
+    PlotE(i,j);
+   end
+end
+
+plot(0,0,'o');
+
+hold off;
---
+++ PlotE.m
@@ -0,0 +1,15 @@
+function[] = PlotE(a,b)     %(a,b) is a point
+    %we need direction(vec) and legnth(num) of arrow
+    D = [a-0,b-0];
+    %assume r is distance from proton to origin satisfied L = 1*(1/r)^2
+    r = (a.^2+b.^2).^(1/2);
+    L = (1./r).^2;
+    
+    %so start point and end point of arrow will be:
+    s = [a,b] - (1/2)*L*D;
+    e = [a,b] + (1/2)*L*D;
+    
+    %using PlotArrow to plot one of arows of the eletric field
+    PlotArrow(s,e);
+
+end
